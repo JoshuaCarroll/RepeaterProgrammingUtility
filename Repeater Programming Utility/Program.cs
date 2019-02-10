f28d4b6545a329fe6314acaff52b6f91551ce2c0
Joshua Carroll <joshcarroll21@gmail.com>
2019-02-09 18:37:47 -0600

    Set SSH form as default (temporary)

--- Repeater Programming Utility/Program.cs
+++ Repeater Programming Utility/Program.cs
@@ -15,7 +15,7 @@ namespace Repeater_Programming_Utility
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new FormSSH());
         }
     }
 }
